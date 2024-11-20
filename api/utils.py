2f069a9d7c6bea740bcdfea93afb97f33e02d2d8
Alihasnat10 <aliistheflash@gmail.com>
2024-11-20 16:22:43 -0500

    Latest code

--- api/utils.py
+++ api/utils.py
@@ -1,12 +1,32 @@
-def analyze_url(url):
-    # Placeholder function for URL analysis
-    # This will be replaced with actual ML model in the future
-    if len(url) > 50:
-        return {"is_phishing": True, "confidence": 0.8}
-    return {"is_phishing": False, "confidence": 0.7}
 
-# Additional functions for feature extraction can be added here
-def extract_features(url):
-    # Placeholder for feature extraction
-    # This function will be implemented when integrating the ML model
-    pass
+import pickle
+# from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+import sklearn
+print(sklearn.__version__)
+vectorizer = joblib.load('api/model_weights/tfidf_vectorizer2.pkl')
+
+with open('api/model_weights/random_forest_model.pkl', 'rb') as model_file:
+    model_rf = pickle.load(model_file)
+
+# with open('api/model_weights/tfidf_vectorizer1.pkl', 'rb') as vectorizer_file:
+#     vectorizer = pickle.load(vectorizer_file)
+
+def analyze_url(url):
+    try:
+        print(hasattr(vectorizer, "idf_"))
+        print(vectorizer)
+        print(url)
+        # Preprocess the URL
+        url_transformed = vectorizer.transform([url])
+        # Make a prediction
+        prediction = model_rf.predict(url_transformed)
+        
+        # Determine if it's phishing and return confidence
+        is_phishing = prediction[0] == 0  # Assuming 0 means phishing
+        confidence = model_rf.predict_proba(url_transformed)[0][1] if is_phishing else model_rf.predict_proba(url_transformed)[0][0]
+        print(confidence)
+        return {"is_phishing": is_phishing, "confidence": confidence, "success":True, "error":""}
+    except Exception as e:
+        print(e)
+        return{"is_phishing": "", "confidence": -1, "success":False, "error":str(e)}
